86913c4c008cec93fa784183cc355e0a656771ed
malyshev.v <malyshev.v@skbkontur.com>
2020-12-15 23:07:31 +0500

    partly done

--- homeworks/homework_6/src/main/scala/Exercises.scala
+++ homeworks/homework_6/src/main/scala/Exercises.scala
@@ -1,7 +1,9 @@
 object Exercises {
 
 
-  def reverse[T](seq: Seq[T]): Seq[T] = ???
+  def reverse[T](seq: Seq[T]): Seq[T] = {
+    seq.reverse
+  }
 
   /**
    * https://ru.wikipedia.org/wiki/Числа_Фибоначчи
@@ -9,9 +11,32 @@ object Exercises {
    * @param idx
    * @return
    */
-  def fibonacci4Index(idx: Int): Int = ???
+  def fibonacci4Index(idx: Int): Int = {
+    var a = 1
+    var b = 1
+    for (_ <- 2 until idx) {
+      val temp = b
+      b = a + b
+      a = temp
+    }
 
-  def fibonacci(idx: Int): Seq[Int] = ???
+    if (idx == 0)
+      return 0
+
+    b
+  }
+
+  def fibonacci(idx: Int): Seq[Int] = {
+    var a = 0;
+    var b = 1;
+    for (_ <- 0 to idx) yield {
+      val temp = b
+      val result = a
+      b = a + b
+      a = temp
+      result
+    }
+  }
 
   lazy val MORSE = Map("A" -> ".-", "B" -> "-...", "C" -> "-.-.", "D" -> "-..", "E" -> ".", "F" -> "..-.",
                        "G" -> "--.", "H" -> "....", "I" -> "..", "J" -> ".---", "K" -> "-.-", "L" -> ".-..",
